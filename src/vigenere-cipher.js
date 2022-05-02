8535e496bf0037e679f4f1b79c69d7557f95f89e
Kiryl1337 <kiryl.kniazeu@gmail.com>
2022-05-03 01:59:59 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+   
+  constructor(isDirect = true) {
+      this.isDirect = isDirect
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key){
+      throw Error("Incorrect arguments!");
+    } 
+    let encryptResult = [];
+    let counter = 0;
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase();
+    for (let i = 0; i < message.length; i++) {
+        if (message[i].charCodeAt(0) >= 65 && message[i].charCodeAt(0) <= 90) {
+          let value = counter++ % key.length
+          let code = message[i].charCodeAt(0) - 65 + key[value].charCodeAt(0)
+          message[i] = String.fromCharCode((code - 65) % 26 + 65);
+        }
+        encryptResult.push(message[i]);
+    }
+    if(this.isDirect){
+      return encryptResult.join('')
+    }
+    return encryptResult.reverse().join('')
+  }
+
+  decrypt(message, key) {
+    if (!message || !key){
+      throw Error("Incorrect arguments!");
+    } 
+    let decryptResult = [];
+    let counter = 0;
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase();
+    for (let i = 0; i < message.length; i++) {
+        if (message[i].charCodeAt(0) >= 65 && message[i].charCodeAt(0) <= 90) {
+          let value = counter++ % key.length
+          let code = message[i].charCodeAt(0) + 65 - key[value].charCodeAt(0)
+          message[i] = String.fromCharCode((code + 65) % 26 + 65);
+        }
+        decryptResult.push(message[i]);
+    }
+    if(this.isDirect){
+      return decryptResult.join('')
+    }
+    return decryptResult.reverse().join('')
   }
 }
 
